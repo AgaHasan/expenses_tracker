8746d2df7921c64749eb9dcb71a59f46f6780203
AgaHasan <bhasanagic1@etf.unsa.ba>
2022-12-06 16:36:28 +0100

    Added Expenses component to simplify app.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,32 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from './Components/Expenses';
 
 function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Registracija",
-      amount: 500,
-      date: new Date(2022, 11, 11),
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id: "e2",
-      title: "Registracija",
-      amount: 550,
-      date: new Date(2021, 11, 11),
-    },
-    {
-      id: "e3",
-      title: "Registracija",
-      amount: 600,
-      date: new Date(2020, 11, 11),
-    },
-    {
-      id: "e4",
-      title: "Registracija",
-      amount: 700,
-      date: new Date(2019, 11, 11),
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
   ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
